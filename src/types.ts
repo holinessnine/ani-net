407ac91af5f2f6da12c37b49ed6a892861f74019
JJEliPark <pkmon111d@gmail.com>
2024-07-20 02:28:22 +0000

    Cluster Label 추가

--- src/types.ts
+++ src/types.ts
@@ -16,24 +16,6 @@ export interface NodeData {
   x: number;
   y: number;
 }
-
-export interface NodeData_c {
-  key: string;
-  label: string;
-  year: string;
-  type: string;
-  top_art: string;
-  top_rank: number;
-  total_art: number;
-  avg_favorites: number;
-  avg_score: number;
-  URL: string;
-  cluster: string;
-  color: string;
-  x: number;
-  y: number;
-}
-
 export interface EdgeData {
   source_year: number;
   source: string;
@@ -43,13 +25,6 @@ export interface EdgeData {
   type: string;
 }
 
-export interface EdgeData_c {
-  year: string;
-  source: string;
-  dest: string;
-  sim_score: number;
-  type: string;
-}
 
 export interface Cluster {
   key: string;
@@ -81,12 +56,60 @@ export interface Dataset {
   ratings: Rating[];
 }
 
+export interface FiltersState {
+  clusters: Record<string, boolean>;
+  tags: Record<string, boolean>;
+  years: Record<number, boolean>;
+  ratings: Record<string, boolean>;
+}
+
+
+export interface NodeData_c {
+  key: string;
+  label: string;
+  year: string;
+  type: string;
+  top_art: string;
+  top_rank: number;
+  total_art: number;
+  avg_favorites: number;
+  avg_score: number;
+  URL: string;
+  cluster: string;
+  color: string;
+  x: number;
+  y: number;
+}
+
+export interface EdgeData_c {
+  year: string;
+  source: string;
+  dest: string;
+  sim_score: number;
+  type: string;
+}
+
+
+export interface Cluster_c {
+  key: string;
+  color: string;
+  clusterLabel: string;
+  x: number;
+  y: number;
+}
+
+export interface Year_c {
+  key: string;
+  clusterLabel: string;
+}
+
+
 export interface Dataset_c {
   nodes: NodeData_c[];
   edges: EdgeData_c[];
-  clusters: Cluster[];
+  clusters: Cluster_c[];
   tags: Tag[];
-  years: Year[];
+  years: Year_c[];
   ratings: Rating[];
 }
 
